cb2575a967c9097a832640e9fea9c9e38c1583fb
Jaroslaw <yahouston99@gmail.com>
2021-03-22 14:47:42 +0100

    First JS lesson

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,22 @@
-let number = 5;
-const leftBorderWidth = 1;
-number = number + 2;
-number = number + 3;
+"use strict";
+
+const numberOfFilms = prompt("How many films did you wach?", "");
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: {},
+};
+
+var filmName = prompt("Whats was the last film wached?", "");  
+var filmRate = prompt("Please rate this film from 1 to 10!", "10");
+personalMovieDB.movies[filmName] = filmRate;
+var filmName = prompt("Whats was the last film wached?", "");  
+var filmRate = prompt("Please rate this film from 1 to 10!", "10");
+personalMovieDB.movies[filmName] = filmRate;
+
+
+console.log(personalMovieDB);
