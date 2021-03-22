a4299b7167ede91046a4c9078ec58cbbb8b476d9
Jaroslaw <yahouston99@gmail.com>
2021-03-22 17:47:53 +0100

    2 Ehjr gj JS

--- JS/script.js
+++ JS/script.js
@@ -11,12 +11,30 @@ const personalMovieDB = {
     private: {},
 };
 
-var filmName = prompt("Whats was the last film wached?", "");  
-var filmRate = prompt("Please rate this film from 1 to 10!", "10");
-personalMovieDB.movies[filmName] = filmRate;
-var filmName = prompt("Whats was the last film wached?", "");  
-var filmRate = prompt("Please rate this film from 1 to 10!", "10");
-personalMovieDB.movies[filmName] = filmRate;
+for(let i = 0; i<2;i++)
+{
+        var filmName = prompt("Whats was the last film wached?", "");
+        var filmRate = +prompt("Please rate this film from 1 to 10!", "10");
 
-
+        if(filmName != ""  && filmRate != "" && filmName.length <50 && filmName != null && filmRate != null )
+        {
+            personalMovieDB.movies[filmName] = filmRate;
+            console.log("Done!");
+        }
+        else
+        { 
+            i--;
+            console.log("Error!");
+        }
+}
+if (numberOfFilms <10)
+{
+    console.log("Маловато фильмов!");
+}else if (numberOfFilms>=10 && numberOfFilms<=30)
+{
+    console.log("Good!");
+}else if (numberOfFilms>30)
+{
+    console.log("Ты киноман !!!");
+}
 console.log(personalMovieDB);
